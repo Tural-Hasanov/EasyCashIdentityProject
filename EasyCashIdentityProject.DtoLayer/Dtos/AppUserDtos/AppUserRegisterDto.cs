3c808b158fbd1c23e20302be8a83e4c732a64287
Tural Hasanov <tural_hasanov@bk.ru>
2023-09-08 14:42:29 +0400

    update

---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        //[Required(ErrorMessage = "Ad daxil edin")]
+        //[Display(Name = "Ad")]
+        //[MaxLength(30,ErrorMessage ="Maksimum 30 xarakter daxil ede bilersiniz")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
